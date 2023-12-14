6fa1cfe6298030acca6b94a27b329ceaeb3c48f3
FaishalAlbadri <faishalalbadri1@gmail.com>
2023-12-14 13:31:42 +0700

    dummy data

--- app/src/main/java/com/faishalbadri/uiismarttv/fragment/home/HomeViewModel.kt
+++ app/src/main/java/com/faishalbadri/uiismarttv/fragment/home/HomeViewModel.kt
@@ -7,9 +7,10 @@ import com.faishalbadri.uiismarttv.data.dummy.Banner
 import com.faishalbadri.uiismarttv.data.dummy.DummyData
 import com.faishalbadri.uiismarttv.data.dummy.HomeData
 import com.faishalbadri.uiismarttv.data.dummy.News
+import com.faishalbadri.uiismarttv.data.dummy.RadioData
 import com.faishalbadri.uiismarttv.data.dummy.Video
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
@@ -17,19 +18,33 @@ class HomeViewModel: ViewModel() {
     private val _contentData = MutableLiveData<List<HomeData>>()
     val contentData: LiveData<List<HomeData>> = _contentData
 
+    private var dataBanner: MutableList<Banner> = ArrayList()
+    private var dataVideo: MutableList<Video> = ArrayList()
+    private var dataNews: MutableList<News> = ArrayList()
+
     fun getContent() {
+        _isLoading.value = true
+
+        dataBanner.addAll(DummyData().dataBanner)
+        dataVideo.addAll(DummyData().dataVideo)
+        dataNews.addAll(DummyData().dataNews)
+
+        dataBanner.add(Banner("0", "", "", "", ""))
+        dataVideo.add(Video("0", "", "", "", ""))
+        dataNews.add(News("0", "", "", ""))
+
         var dataResponse: List<HomeData> = listOf(
             HomeData(
                 msg = HomeData.Banner,
-                list = DummyData().dataBanner
+                list = dataBanner
             ),
             HomeData(
                 msg = HomeData.Video,
-                list = DummyData().dataVideo
+                list = dataVideo
             ),
             HomeData(
                 msg = HomeData.News,
-                list = DummyData().dataNews
+                list = dataNews
             )
         )
         _contentData.value = dataResponse
